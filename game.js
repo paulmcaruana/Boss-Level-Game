490bc5acefc5521fef4380110b6b384804c0c7a4
Paul  Caruana <201901806@itptstudent.co.uk>
2020-02-20 16:39:38 +0000

    end of class commit

--- game.js
+++ game.js
@@ -1,20 +1,57 @@
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var randomNumber;
+var randomChosenColor;
 //var randomChosenColor;
 
 nextSequence();
 
 function nextSequence(){
-var randomNumber = Math.floor((Math.random() * 3) + 1);
-var randomChosenColor = buttonColors[randomNumber];
+var randomNumber = Math.floor((Math.random() * 4));
+
+randomChosenColor = buttonColors[randomNumber];
+console.log(randomChosenColor);
 gamePattern.push(randomChosenColor);
 
-
-//flashColors();
-
-//function flashColors(){
-$("#" + randomChosenColor).bind(randomChosenColor, function(){
-$("#" + randomChosenColor).fadeout(500).fadein(500);
-});
+$("#" + randomChosenColor).fadeOut(500).fadeIn(500);
 }
+
+makeSound();
+
+function makeSound(randomChosenColor){
+    switch (randomChosenColor) {
+      case "green":
+      var green = new Audio('sounds/green.mp3');
+    green.play();
+        break;
+
+        case "blue":
+        var blue = new Audio('sounds/blue.mp3');
+      blue.play();
+          break;
+
+          case "red":
+          var red = new Audio('sounds/red.mp3');
+        red.play();
+            break;
+
+            case "yellow":
+            var yellow = new Audio('sounds/yellow.mp3');
+          yellow.play();
+              break;
+
+  }
+}
+
+
+
+/* $(document).ready(function() {
+       var obj = document.createElement("audio");
+       obj.setAttribute("src", "http://kahimyang.info/resources/click.m...
+       $.get();
+
+       $(".playSound").click(function() {
+           obj.play();
+       });
+   });
+   */
