ac741270e3e7eb4fdb2baf6865762f9fe67ee68e
Paul  Caruana <201901806@itptstudent.co.uk>
2020-02-20 15:28:40 +0000

    initial attempt at flashing code

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("do you like this");
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomNumber;
+//var randomChosenColor;
+
+nextSequence();
+
+function nextSequence(){
+var randomNumber = Math.floor((Math.random() * 3) + 1);
+var randomChosenColor = buttonColors[randomNumber];
+gamePattern.push(randomChosenColor);
+}
+
+//flashColors();
+
+var buttonFlash = randomChosenColor;
+//function flashColors(){
+$("#" + buttonFlash).bind(buttonFlash, function(){
+$("#" + buttonFlash).fadeout(500).fadein(500);
+});
+//}
